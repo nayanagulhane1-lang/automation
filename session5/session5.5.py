859c7ba9e2047d7187156bbb6deb1a61c767a7f6
nayanagulhane1-lang <nayanagulhane1@gmail.com>
2026-08-11 15:15:24 +0530

    Create session5.5.py
    session5 task

---
+++ session5/session5.5.py
@@ -0,0 +1,6 @@
+#Use a nested for loop to print a pattern of stars forming a right-angled triangle with 4 rows
+# (like the rating stars you see on Zomato).
+# <br><br><em><strong>Constraint:</strong> Only use for loops and range(), 
+# no string multiplication.</em>
+for i in range(1,5):
+    print("*"*i)   
