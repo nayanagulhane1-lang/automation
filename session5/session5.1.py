780165179c02abe6c969fd74f7e56de5036085e0
nayanagulhane1-lang <nayanagulhane1@gmail.com>
2026-08-11 15:12:55 +0530

    Create session5.1.py
    session5 task

---
+++ session5/session5.1.py
@@ -0,0 +1,6 @@
+#Write a Python script that prints each item in the list ['Zomato', 'Swiggy', 'Flipkart', 'Myntra'] 
+# on a new line using a for loop.
+
+item=['Zomato', 'Swiggy', 'Flipkart', 'Myntra'] 
+for i in item:
+ print(i)
