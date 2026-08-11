40c1c447b2e67f41b289478d618246d89888d344
nayanagulhane1-lang <nayanagulhane1@gmail.com>
2026-08-11 15:14:54 +0530

    Create session5.4.py
    session5 task

---
+++ session5/session5.4.py
@@ -0,0 +1,7 @@
+#Build a script that takes a user's name as input and prints a welcome message 
+# 5 times using a for loop with range().<br><br><em><strong>Hint:</strong> 
+# Use input() to get the name and range(5) in your loop.</em>
+
+name=input("Enter user's name:")
+for i in range(5):
+    print("Welcome",name)
