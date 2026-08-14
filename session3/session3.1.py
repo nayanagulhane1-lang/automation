42324d55ce6a74f014cb6dd49aecb72a9c8d7eea
nayanagulhane1-lang <nayanagulhane1@gmail.com>
2026-08-14 16:51:55 +0530

    Create session3.1.py
    session 3 task

---
+++ session3/session3.1.py
@@ -0,0 +1,6 @@
+#Write a Python script that asks the user for their name and favorite IPL team, then prints a welcome message using an f-string, such as 'Welcome, Priya! Your favorite team is Mumbai Indians.'
+
+name = input("Enter your name: ")
+favorite_team = input("Enter your favorite IPL team: ")
+
+print(f"Welcome, {name}! Your favorite team is {favorite_team}.")
