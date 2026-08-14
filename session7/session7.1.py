5f8ad5b1e97457c4298673051da0c0858bb77a46
nayanagulhane1-lang <nayanagulhane1@gmail.com>
2026-08-14 14:07:51 +0530

    Create session7.1.py
    session 7 task

---
+++ session7/session7.1.py
@@ -0,0 +1,4 @@
+#Given the string 'Your Zomato order ID is: ZMT12345. Enjoy your meal!', extract only the order ID (e.g., 'ZMT12345') using string slicing and indexing.
+my_string = "Your Zomato order ID is: ZMT12345. Enjoy your meal!"
+
+print(my_string[25:33])
