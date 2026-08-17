ff251e69253914796916707042274a5f8cb1d390
nayanagulhane1-lang <nayanagulhane1@gmail.com>
2026-08-17 17:04:15 +0530

    Create session9.1.py
    session9 task

---
+++ session9/session9.1.py
@@ -0,0 +1,9 @@
+#Create a Python list called playlist containing the names of your 5 favorite songs as strings and
+# print the entire list.
+playlist=['lag ja gale ke fir ye rat ho na ho',
+          'aj se jane man dil hai tumhara',
+          'yu to hum tum milene ko milte rahe koi mil gaya',
+          'bole chudiyan bole kangana hy mai ho gayi teri',
+           'pehla nasha pehla khumar']
+
+print(playlist)
