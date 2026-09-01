43f5881ee3db53a460dc517aeaca2df2770b3b69
nayanagulhane1-lang <nayanagulhane1@gmail.com>
2026-09-01 10:58:12 +0530

    Create session10.1.py
    session10 task

---
+++ session10/session10.1.py
@@ -0,0 +1,8 @@
+#Create a tuple named fav_brands containing the names of 5 clothing brands you see on Myntra,
+# and print the second and last brand from the tuple.
+fav_brands=("kurti","t-shirts","salwar","jeans","top")
+
+
+print(fav_brands[1])
+print(fav_brands[4])
+    
